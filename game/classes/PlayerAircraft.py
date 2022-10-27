24800a5240826fc777083cb2743bf93a21dd8ae6
jaeymax <azagojunior2@gmail.com>
2022-10-27 18:07:30 -0400

    added the player shooting functionality

--- game/classes/PlayerAircraft.py
+++ game/classes/PlayerAircraft.py
@@ -2,21 +2,30 @@ from game.classes.button import Button
 from .aircraft import Aircraft
 from .bullet import Bullet
 from typing import List
+import time
 
 class PlayerAircraft(Aircraft):
+    BULLET_COOLDOWN_TIME:int = 0.10
+
 
     def __init__(self, name: str, imagePath: str, pos: tuple[int, int]):
         super().__init__(name, imagePath, pos)
-        self.speed = 3
+        self.speed = 5
         self.bullets:List = []
-
+        self.can_shoot = True
+        self.last_cooldown_time = time.time()
 
     def shoot(self) -> None:
-        bullet  = Bullet((self.img_rect.x, self.img_rect.y), "laser4.png")
-        self.bullets.append(bullet)
+        if self.can_shoot:
+            bullet  = Bullet((self.img_rect.x, self.img_rect.y), "laser4.png", "laser3.wav")
+            self.bullets.append(bullet)
 
     def update(self, surface) -> None:
-        pass
+        if time.time() - self.last_cooldown_time >= PlayerAircraft.BULLET_COOLDOWN_TIME:
+            self.can_shoot = True
+            self.last_cooldown_time = time.time()
+        else:
+            self.can_shoot = False    
         # for bullet in self.bullets:
         #     bullet.update()
         #     bullet.draw(surface)    
--- game/classes/bullet.py
+++ game/classes/bullet.py
@@ -4,12 +4,14 @@ from game.classes.constants import Constants
 
 class Bullet:
 
-    def __init__(self, pos:tuple[int, int], imagePath:str) -> None:
+    def __init__(self, pos:tuple[int, int], imagePath:str, soundPath:str) -> None:
         self.image = pygame.image.load(imagePath).convert_alpha()
         self.img_rect = self.image.get_rect()
         self.img_rect.x = pos[0]
         self.img_rect.y = pos[1]
         self.speed = 8
+        self.sound = pygame.mixer.Sound(soundPath)
+        self.sound.play()
 
     def draw(self, surface)->None:
         surface.blit(self.image, self.img_rect)
